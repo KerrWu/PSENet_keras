d4c8b03187169914afb9febcb208ed074819ddd7
KerrWu <wz_0818@163.com>
2019-07-24 18:00:42 +0800

    fpn added

--- SR_Module.py
+++ SR_Module.py
@@ -4,56 +4,56 @@ from keras import regularizers
 
 
 def score_refine_module(input_feature_map, map_name=None):
-    global weight_decay
+    global myModelConfig
 
     with tf.variable_scope("score_refine_module", reuse=tf.AUTO_REUSE):
-        x = Conv2D(256, (1, 1), activation="relu", kernel_regularizer=regularizers.l2(weight_decay))(input_feature_map)
+        x = Conv2D(256, (1, 1), activation="relu", kernel_regularizer=regularizers.l2(myModelConfig.weight_decay))(input_feature_map)
 
         # score head:
         score_map_s1 = Conv2D(256, (1, 1), padding='same', activation="relu", kernel_initializer='he_normal',
-                              kernel_regularizer=regularizers.l2(weight_decay))(x)
+                              kernel_regularizer=regularizers.l2(myModelConfig.weight_decay))(x)
         score_map_s2 = Conv2D(256, (3, 3), padding='same', activation="relu", kernel_initializer='he_normal',
-                              kernel_regularizer=regularizers.l2(weight_decay))(x)
+                              kernel_regularizer=regularizers.l2(myModelConfig.weight_decay))(x)
         score_map_s1s2 = Add()([score_map_s1, score_map_s2])
 
         score_map_s1 = Conv2D(256, (1, 1), padding='same', activation="relu", kernel_initializer='he_normal',
-                              kernel_regularizer=regularizers.l2(weight_decay))(score_map_s1s2)
+                              kernel_regularizer=regularizers.l2(myModelConfig.weight_decay))(score_map_s1s2)
         score_map_s2 = Conv2D(256, (3, 3), padding='same', activation="relu", kernel_initializer='he_normal',
-                              kernel_regularizer=regularizers.l2(weight_decay))(score_map_s1s2)
+                              kernel_regularizer=regularizers.l2(myModelConfig.weight_decay))(score_map_s1s2)
         score_map_s1s2 = Add()([score_map_s1, score_map_s2])
 
         score_map_s1 = Conv2D(256, (1, 1), padding='same', activation="relu", kernel_initializer='he_normal',
-                              kernel_regularizer=regularizers.l2(weight_decay))(score_map_s1s2)
+                              kernel_regularizer=regularizers.l2(myModelConfig.weight_decay))(score_map_s1s2)
         score_map_s2 = Conv2D(256, (3, 3), padding='same', activation="relu", kernel_initializer='he_normal',
-                              kernel_regularizer=regularizers.l2(weight_decay))(score_map_s1s2)
+                              kernel_regularizer=regularizers.l2(myModelConfig.weight_decay))(score_map_s1s2)
         score_map_s1s2 = Add()([score_map_s1, score_map_s2])
 
         score_map_s1 = Conv2D(256, (1, 1), padding='same', activation="relu", kernel_initializer='he_normal',
-                              kernel_regularizer=regularizers.l2(weight_decay))(score_map_s1s2)
+                              kernel_regularizer=regularizers.l2(myModelConfig.weight_decay))(score_map_s1s2)
         score_map_s2 = Conv2D(256, (3, 3), padding='same', activation="relu", kernel_initializer='he_normal',
-                              kernel_regularizer=regularizers.l2(weight_decay))(score_map_s1s2)
+                              kernel_regularizer=regularizers.l2(myModelConfig.weight_decay))(score_map_s1s2)
         score_map_s1s2 = Add()([score_map_s1, score_map_s2])
 
         score_map = Conv2D(5, (1, 1), padding='same', activation="relu", kernel_initializer='he_normal',
-                           kernel_regularizer=regularizers.l2(weight_decay))(score_map_s1s2)
+                           kernel_regularizer=regularizers.l2(myModelConfig.weight_decay))(score_map_s1s2)
 
         # locate head
         locate_head = Conv2D(256, (1, 1), padding='same', activation="relu", kernel_initializer='he_normal',
-                             kernel_regularizer=regularizers.l2(weight_decay))(x)
+                             kernel_regularizer=regularizers.l2(myModelConfig.weight_decay))(x)
         locate_head = Conv2D(256, (1, 1), padding='same', activation="relu", kernel_initializer='he_normal',
-                             kernel_regularizer=regularizers.l2(weight_decay))(locate_head)
+                             kernel_regularizer=regularizers.l2(myModelConfig.weight_decay))(locate_head)
         locate_head = Conv2D(256, (1, 1), padding='same', activation="relu", kernel_initializer='he_normal',
-                             kernel_regularizer=regularizers.l2(weight_decay))(locate_head)
+                             kernel_regularizer=regularizers.l2(myModelConfig.weight_decay))(locate_head)
         locate_head = Conv2D(256, (1, 1), padding='same', activation="relu", kernel_initializer='he_normal',
-                             kernel_regularizer=regularizers.l2(weight_decay))(locate_head)
+                             kernel_regularizer=regularizers.l2(myModelConfig.weight_decay))(locate_head)
 
         if map_name == None:
             locate_map = Conv2D(1, (1, 1), padding='same', activation="sigmoid", kernel_initializer='he_normal',
-                                kernel_regularizer=regularizers.l2(weight_decay))(locate_head)
+                                kernel_regularizer=regularizers.l2(myModelConfig.weight_decay))(locate_head)
         else:
             locate_map = Conv2D(1, (1, 1), padding='same', activation="sigmoid", kernel_initializer='he_normal',
                                 name=map_name + "_locate",
-                                kernel_regularizer=regularizers.l2(weight_decay))(locate_head)
+                                kernel_regularizer=regularizers.l2(myModelConfig.weight_decay))(locate_head)
 
         refined_map = Multiply()([score_map, locate_map])
 
